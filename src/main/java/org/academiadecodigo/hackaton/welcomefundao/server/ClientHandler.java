262a5dc0d8debdf81831b986ea2cff7c2ab73276
Eurico Fonseca <eurico5@sapo.pt>
2016-11-24 17:16:54 +0000

    added server and first version of the client handler

--- src/main/java/org/academiadecodigo/hackaton/welcomefundao/server/ClientHandler.java
+++ src/main/java/org/academiadecodigo/hackaton/welcomefundao/server/ClientHandler.java
@@ -1,7 +1,73 @@
 package org.academiadecodigo.hackaton.welcomefundao.server;
 
+import java.io.*;
+import java.net.Socket;
+
 /**
  * Created by codecadet on 24/11/16.
  */
-public class ClientHandler {
+public class ClientHandler implements Runnable, Serializable{
+    private ObjectOutputStream out;
+    private Socket clientSocket;
+
+    public ClientHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+
+            ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
+            out = new ObjectOutputStream(clientSocket.getOutputStream());
+
+            //UserService userService = new MockUserService();
+
+
+            Object readingObj;
+
+            while (true) {
+                //wait for Client request
+                readingObj = in.readObject();
+
+                if (readingObj == null) {
+                    System.out.println("closing connection");
+                    return;
+                }
+
+                if (readingObj instanceof String) {
+
+                    String[] userLogin = ((String) readingObj).split("[.]");
+                    System.out.println("\n --Auth attempt-- ");
+                    System.out.println("username: " + userLogin[0] + "\npassword: " + userLogin[1]+"\n");
+
+                }
+
+
+            }
+
+
+        } catch (EOFException e) {
+            System.out.println("Server closed the connection");
+            System.exit(1);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Unknown object");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void send(Object obj){
+        try {
+            out.reset(); //To avoid accumulating the buffer, the buffer shall be reset before writing in it.
+            out.writeObject(obj);
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/org/academiadecodigo/hackaton/welcomefundao/server/Server.java
+++ src/main/java/org/academiadecodigo/hackaton/welcomefundao/server/Server.java
@@ -1,7 +1,44 @@
 package org.academiadecodigo.hackaton.welcomefundao.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by codecadet on 24/11/16.
  */
 public class Server {
+    private int portNumber;
+
+    public Server(int portNumber) {
+        this.portNumber = portNumber;
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server(2000);
+
+        server.start();
+
+    }
+
+    private void start() {
+
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            while (true) {
+                //wait for new client connection
+                System.out.println("Waiting for connection");
+                Socket clientSocket = serverSocket.accept();
+
+
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
