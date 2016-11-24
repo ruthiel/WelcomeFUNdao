b00e6cba802e51d5f68a3da9c2d615450b5f5961
Eurico Fonseca <eurico5@sapo.pt>
2016-11-24 16:07:39 +0000

    add ConnectionManager

--- src/main/java/org/academiadecodigo/hackaton/welcomefundao/persitence/ConnectionManager.java
+++ src/main/java/org/academiadecodigo/hackaton/welcomefundao/persitence/ConnectionManager.java
@@ -1,7 +1,59 @@
 package org.academiadecodigo.hackaton.welcomefundao.persitence;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by codecadet on 24/11/16.
  */
 public class ConnectionManager {
+
+    private static final String DEFAULT_USER = "root";
+    private static final String DEFAULT_PASS = "";
+    private static final String DEFAULT_HOST = "localhost";
+    private static final String DEFAULT_DB = "welcomefundao";
+
+    private static final String CONNECTOR = "jdbc:mysql:";
+
+    private String dbURL;
+    private String user;
+    private String pass;
+    private Connection connection;
+
+    public ConnectionManager() {
+        this(DEFAULT_USER, DEFAULT_PASS, DEFAULT_HOST, DEFAULT_DB);
+    }
+
+    public ConnectionManager(String user, String pass, String host, String dataBase) {
+        this.user = user;
+        this.pass = pass;
+        this.dbURL = CONNECTOR + "//" + host + "/" + dataBase;
+    }
+
+    public Connection getConnection() {
+        connection = null;
+
+        try {
+            System.out.println("Making connection...");
+
+            if (connection == null) {
+                connection = DriverManager.getConnection(dbURL, user, pass);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Fail to connect to database!" + ex.getMessage());
+        }
+        return connection;
+    }
+
+    public void close() {
+        try {
+            if(connection != null) {
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            System.out.println("Fail to connect!");
+        }
+    }
+
 }
